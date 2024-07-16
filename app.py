52722db4ae471f5e2362dd2eeec3f892661a43c8
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2024-07-16 12:17:20 +0530

    Add hello endpoint  for  testing

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from twilio.rest import Client
 load_dotenv()
 
 app = Flask(__name__)
-client = Client()
+client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
 
 def respond(message):
     response = MessagingResponse()
@@ -19,3 +19,10 @@ def reply():
     message = request.form.get('Body').lower()
     if message:
         return respond(f'Thank you for your message! A member of our team will be in touch with you soon.')
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return 'Hello! Welcome !'
+
+if __name__ == '__main__':
+    app.run(debug=True)
