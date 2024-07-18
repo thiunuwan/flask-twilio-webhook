bca6473efb903a9a4e4d5cce594fecdc2b32b51b
Ravindu Thiunuwan <83197833+thiunuwan@users.noreply.github.com>
2024-07-18 10:10:46 +0530

    Update iv

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 app = Flask(__name__)
-
 load_dotenv()
+client = Client()
 
-app = Flask(__name__)
-client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
+# app = Flask(__name__)
+# client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
 
 def respond(message):
     response = MessagingResponse()
@@ -19,7 +19,7 @@ def respond(message):
 def reply():
     message = request.form.get('Body').lower()
     if message:
-        return respond(f'Thank you for your message! A member of our team will be in touch with you soon.')
+        return respond(f'Thank you for your message: "{message}". A member of our team will be in touch with you soon.')
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
